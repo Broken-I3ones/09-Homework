2bc8975b84b392ce61fa53f5d41b24e638bfc37e
Broken-I3ones <80986875+Broken-I3ones@users.noreply.github.com>
2021-07-19 20:48:22 -0600

    Added questions to prompt the user

--- Develop/index.js
+++ Develop/index.js
@@ -1,118 +1,96 @@
 // TODO: Include packages needed for this application
-const fs = require("fs");
-const axios = require("axios");
 const inquirer = require("inquirer");
+const fs = require("fs");
+const { massage } = require(`statuses`)
+const axios = require("axios");
 const markdown = require("./utils/generateMarkdown");
+const { Console } = require("console");
 // TODO: Create an array of questions for user input
-const questions = [
-    {   
-        message: "What is the name of the project?",
-        name: "title"
-    },
-    {   
-        message: "Please provide a table on content",
-        name: "table of content"
-    },
-    {   
-        message: "What is the name of the user?",
-        name: "userName"
-    },
-    {  
-        message: "Please provide a description of the project",
-        name: "description"
-    },
-    {   
-        message: "What is the installation process?",
-        name: "installation"
-    },
-    {   
-        message: "How will this project be used?",
-        name: "usage"
-    },
-    {   
-        message: "What licenses are required with this project?",
-        name: "licenses"
-    },
-    {   
-        message: "Who were the contributors to this project?",
-        name: "contribution"
-    },
-    {   
-        message: "What is the test process for this project?",
-        name: "test"
-    },
-    {   
-        message: "What is the user github email address?",
-        name: "GitHub user email"
-    },
-    {   
-        message: "Please provide a profile picture",
-        name: "GitHub profile picture"
-    }
-]
-// TODO: Create a function to initialize app
-function init () {
-    inquirer.prompt(questions)
-    .then((inquirerResponse, data) => {   
-        console.log("Making ReadMe");
-        fs.writeFileSync("ReadMe.md", inquirerResponse, data);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+inquirer.prompt(
+    [
+        {   
+            type: "input",
+            massage: "What's the project title?",
+            name: "title",
+            validate: (value)=>{ if(value){return true} else {return `I need a value to continue`}}
+        },
+        {   
+            type: "input",
+            message: "How do you install your app?",
+            name: "installation",
+            validate: (value)=>{ if(value){return true} else {return `I need a value to continue`}}
+        },
+        {   
+            type: "input",
+            message: "How do you use your app?",
+            name: "instructions",
+            validate: (value)=>{ if(value){return true} else {return `I need a value to continue`}}
+        },
+        {   
+            type: "input",
+            message: "Any credits?",
+            name: "credits",
+            validate: (value)=>{ if(value){return true} else {return `I need a value to continue`}}
+        },
+        {   
+            type: "list",
+            message: "What license did you use?",
+            name: "license",
+            choices: ["The MIT Licence", "The GPL License", "Apache License", "GNU license", "N/A"],
+            validate: (value)=>{ if(value){return true} else {return `I need a value to continue`}}
+        },
+        {   
+            type: "input",
+            message: "Github username:",
+            name: "git",
+            validate: (value)=>{ if(value){return true} else {return `I need a value to continue`}}
+        },
+        {   
+            type: "input",
+            message: "What is the user github email address?",
+            name: "email",
+            validate: (value)=>{ if(value){return true} else {return `I need a value to continue`}}
+        },
+    ]
+).then(({
+    title,
+    installation,
+    instructions,
+    credits,
+    license,
+    git,
+    email,
+})=>{
+const template = `# ${title}
+
+* [installation](#installation)
+* [usage](#usage)
+* [licenses](#licenses)
+* [credits](#credits)
+# installation:
+${installation}
+## usage
+${usage}
+### instructions
+${instructions}
+## credits
+${credits}
+##license
+${license}
+
+#Contact
+* github :${git}
+* email :${email}`;
+
+createnewfile(title,template);
 }
-// Function call to initialize app
-init();
+);
 
-const userName = questions.userName
-
-axios.get(`https://api.github.com/users/${userName}`)
-.then(questions => {
-  console.log(questions.data);
-});
-
-function generateMarkdown(response) {
-    return `
-  
-  # ${response.title}
-  
-  # Table of Content
-  -[description](#description)
-  -[installation](#installation)
-  -[usage](#usage)
-  -[licenses](#licenses)
-  -[contribution](#contribution)
-  -[test](#test)
-  -[username](#username)
-  -[profile](#profile)
-  
-  ${response.username}
-  ##username:
-  
-      ${response.description}
-  ##description:
-  
-      ${response.installation}
-  ##installation:
-  
-      ${response.usage}
-  ##usage:
-  
-      ${response.licenses}
-  ##licenses:
-  
-      ${response.contribution}
-  ##contribution:
-  
-      ${response.test}
-  ##test:
-  
-      ${response.email}
-  ##email:
-  
-      ${response.profile}
-  ##profile:
-  `;
-  }
-  
-  module.exports = generateMarkdown;
+function createnewfile(filename,data){
+fs.writeFile(`./${filename.tolowercase().split(``).join('')}.md`,data,(eer)=>{
+    if(eer){
+        console.log(eer)
+    }
+    console.log(`your README has been generated.`);
+})
+}
